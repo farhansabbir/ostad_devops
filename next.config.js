2535f6de248def3ef6c3a96e19a208ffec4ac1e3
Farhan Sabbir Siddique <fsabbir@gmail.com>
2025-07-20 04:40:02 -0600

    renamed file from mjs to js to minimize error checks from sonar

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
   images: {
     domains: ['assets.aceternity.com'],
   },
-  // Removed the problematic 'New Code' line here
   output: 'export', // This tells Next.js to produce a static HTML export to the 'out' directory
-
   // Optionally, you might need to configure basePath if serving from a subdirectory
-
   // basePath: '/my-app',
-
 };
 
-// Change this line to use ES Module export syntax
-export default nextConfig;
+// IMPORTANT: Use module.exports for next.config.js
+module.exports = nextConfig;
