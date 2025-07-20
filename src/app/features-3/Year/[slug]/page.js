c1bed5676ac2653c727b6930627e8eb37efa30cb
Farhan Sabbir Siddique <fsabbir@gmail.com>
2025-07-20 13:49:46 -0600

    reverted page.js

--- src/app/features-3/Year/[slug]/page.js
+++ src/app/features-3/Year/[slug]/page.js
@@ -1,17 +1,4 @@
 "use client";
-
-export async function generateStaticParams() {
-  // Generate params for the last 12 months as an example
-  const months = Array.from({ length: 12 }, (_, i) => {
-    const d = new Date();
-    d.setMonth(d.getMonth() - i);
-    const year = d.getFullYear();
-    const month = (d.getMonth() + 1).toString().padStart(2, '0');
-    return { slug: `${year}-${month}` };
-  });
- 
-  return months;
-}
 import { ArrowLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
