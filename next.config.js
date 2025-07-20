d65ecb4b2d583cfb425f3d6a7e9e8480f9a481c7
Farhan Sabbir Siddique <fsabbir@gmail.com>
2025-07-20 04:47:01 -0600

    exclusion added

--- next.config.js
+++ next.config.js
@@ -3,10 +3,7 @@ const nextConfig = {
   images: {
     domains: ['assets.aceternity.com'],
   },
-  output: 'export', // This tells Next.js to produce a static HTML export to the 'out' directory
-  // Optionally, you might need to configure basePath if serving from a subdirectory
-  // basePath: '/my-app',
+  output: 'export',
 };
 
-// IMPORTANT: Use module.exports for next.config.js
 module.exports = nextConfig;
